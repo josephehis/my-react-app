e52fe44da379cac28058fd61a3aa064dea28d4b0
josephehis <101218648+josephehis@users.noreply.github.com>
2022-09-29 18:18:24 +0100

    Update DisplayFormValue.js

--- src/DisplayFormValue.js
+++ src/DisplayFormValue.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const DisplayFormValue= ({Name, StudentClass, Age})=>{
 
-    // const {Name, StudentClass, Age}= props
+    
     return(
         <div>
             <div>Name:{Name}</div>
